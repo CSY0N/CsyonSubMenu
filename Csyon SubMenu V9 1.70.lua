f3f4d7ac5d3860566de082a8d5ca965eb0ca8efc
CSY0N <134310111+CSY0N@users.noreply.github.com>
2025-04-20 00:59:09 +0200

    Update Csyon SubMenu V9.1 1.70.lua
    Small Update for my SubMenu

--- Csyon SubMenu V9 1.70.lua
+++ Csyon SubMenu V9 1.70.lua
@@ -381,8 +381,8 @@ gui.show_message("Csyon SubMenu", "Csyon Script loaded successfully!")
 
 CSYON = gui.get_tab("**CSYON SubMenu 1.70**					  	**Created By CSYON**")
 
-CSYON:add_text("		Game Build Version 3442		")
-CSYON:add_text("					v9				")
+CSYON:add_text("		Game Build Version 3504		")
+CSYON:add_text("					v9.1				")
 
 CSYON:add_text("Your level                " .. stats.get_int(MPX() .. "CHAR_RANK_FM"))
 
